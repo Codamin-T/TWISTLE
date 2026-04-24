e239de0068ccba58fab3af2589413f2209c88eef
Heba <heba.sadlah@hotmail.com>
2026-04-24 11:28:42 +0200

    small commit before pull

--- src/main/java/com/example/twistle/TwistleApplication.java
+++ src/main/java/com/example/twistle/TwistleApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TwistleApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TwistleApplication.class, args);
     }
